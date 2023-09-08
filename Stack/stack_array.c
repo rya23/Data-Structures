0cfe270a99eaf0f36058016f6e90107dd4542365
Aarya <aaryathakur48@gmail.com>
2023-09-08 19:32:09 +0530

    Completed Stack using Array

--- Stack/stack_array.c
+++ Stack/stack_array.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct student
 {
@@ -31,12 +32,11 @@ void push(struct stack *s, struct student *stud)
 void peek(struct stack *s)
 {
     struct student stud = s->array[s->top];
-    printf("\nName : %s ", stud.name);
-    printf("\nDivision : %d", stud.div);
-    printf("\nRoll No. : %d", stud.rollno);
+    printf("Name : %s\nDivision : %c \nRoll No. : %d", stud.name, stud.div, stud.rollno);
 };
 void pop(struct stack *s)
-{   printf("\nPopped Values are : \n");
+{
+    printf("\nPopped Values are : \n");
     peek(s);
     s->top--;
 }
@@ -50,7 +50,7 @@ int main()
     while (1)
     {
         int num;
-        printf("\n1. Check empty  2. Check full  3. Peek   4. Push  5. Pop  6. Exit\n");
+        printf("\n1. Check empty  2. Check full  3. Push   4. Pop  5. Peek  6. Exit\n");
         scanf("%d", &num);
 
         switch (num)
@@ -77,6 +77,35 @@ int main()
         }
         case 3:
         {
+            if (isfull(s1))
+                printf("Stack is Full. Can't PUSH");
+            else
+            {
+                stud = (struct student *)malloc(sizeof(struct student));
+                printf("Enter Student Details \n");
+                getchar();
+                printf("Name : ");
+                fgets(stud->name, sizeof(stud->name), stdin);
+                printf("Division : ");
+                scanf(" %c", &(stud->div));
+                printf("Roll No. : ");
+                scanf("%d", &(stud->rollno));
+                push(s1, stud);
+            }
+            break;
+        }
+        case 4:
+        {
+            if (isempty(s1))
+                printf("Stack is Empty. Can't POP");
+            else
+            {
+                pop(s1);
+            }
+            break;
+        }
+        case 5:
+        {
             if (isempty(s1))
                 printf("Stack is Empty");
             else
@@ -85,36 +114,10 @@ int main()
             }
             break;
         }
-        case 4:
-        {
-            if (isfull(s1))
-                printf("Stack is Full Cant PUSH");
-            else
-            {
-                stud = (struct student *)malloc(sizeof(struct student));
-                printf("Enter Student Details \n");
-                printf("Name : ");
-                scanf("%s", &(stud->name));
-                printf("Division : ");
-                scanf("%d", &(stud->div));
-                printf("Roll No. : ");
-                scanf("%d", &(stud->rollno));
-                push(s1, stud);
-            }
-            break;
-        }
-        case 5:
-        {
-            if (isempty(s1)) printf("Stack is Empty Cant POP");
-            else
-            {
-                pop(s1);
-            }
-            break;
-        }
 
         case 6:
             return 0;
+        default : printf("\nInvalid Input\n");
         }
     }
 }
